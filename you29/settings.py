dc8d4d7e543249cddc53600260b9fabe45733f44
hideki <h.itakura@yahoo.com>
2008-09-25 01:25:28 -0700

    Fix mail issue

--- you29/settings.py
+++ you29/settings.py
@@ -27,6 +27,10 @@ DATABASE_PASSWORD = '290303'   # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
+
+#Default e-mail address to use for various automated correspondence from the site manager(s).
+DEFAULT_FROM_EMAIL = 'mail@mail.you29.com'
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
