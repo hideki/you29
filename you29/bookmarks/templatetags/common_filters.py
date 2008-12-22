c2032110cb6fb6e90d01498218546ab5b6bbd1f6
hideki <h.itakura@yahoo.com>
2008-12-21 23:33:43 -0800

    Fix bug

--- you29/bookmarks/templatetags/common_filters.py
+++ you29/bookmarks/templatetags/common_filters.py
@@ -1,11 +1,14 @@
+import logging;
 from django import template
 register = template.Library();
 
 @register.filter("truncate_chars")
 def truncate_chars(value, max_length):
+    logging.debug("value="+value + " max_length="+str(max_length));
     if len(value) > max_length:
-        truncd_val = value[:max_length]
-        if value[max_length+1] != " ":
-            truncd_val = truncd_val[:truncd_val.rfind(" ")]
+        logging.debug("len="+str(len(value)));
+        truncd_val = value[:max_length:]
+        #if value[max_length+1] != " ":
+        #    truncd_val = truncd_val[:truncd_val.rfind(" ")]
         return  truncd_val + "..."
     return value
