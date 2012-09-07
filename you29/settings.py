7ddbbf6e8261943ddca832d15063a1c9f2d9bd90
hideki <h.itakura@yahoo.com>
2012-09-06 22:17:33 -0700

    update

--- you29/settings.py
+++ you29/settings.py
@@ -22,8 +22,8 @@ MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'mysql'      # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'you29'        # Or path to database file if using sqlite3.
-DATABASE_USER = 'you29'        # Not used with sqlite3.
-DATABASE_PASSWORD = '290303'   # Not used with sqlite3.
+DATABASE_USER = '-----'        # Not used with sqlite3.
+DATABASE_PASSWORD = '------'   # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
